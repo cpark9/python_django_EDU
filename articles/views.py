b74e17ac2f13ae79be49de1d7aec18341c655a90
chul Park <chulpark@chului-MacBookPro.local>
2025-08-09 18:37:38 +0900

    2025.8.9

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# from django.shortcuts import get_object_or_404
+import random
 
-# Create your views here.
+# Create your views here
+def index(request):
+    return render(request, 'index.html')
+
+def dinner(request, name):
+    menus = [{"name":'족발',"price":30000}, {"name":'햄버거',"price":5000}, {"name":'치킨',"price":20000}, {"name":'초밥',"price":15000}]
+    pick = random.choice(menus)
+    context = {
+        'pick': pick,
+        'name': name,
+        'menus' : menus
+    }
+    return render(request, 'dinner.html', context)
+
+def review(request):
+    return render(request, 'review.html')
+
+def create_review(request):
+    content = request.POST.get('content')
+    print(request.POST)
+    content = {
+        'content': content,
+    }
+    return render(request, 'review_result.html', content)
